34bf54b03cb09db9a1ead612cd89a12a186c3f5e
leenelmir <leenelmirr@gmail.com>
2023-11-19 16:49:20 +0200

    added createDB function

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.cultura {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
     opens com.cultura to javafx.fxml;
     exports com.cultura;
