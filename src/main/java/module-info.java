c8b75e21ccd03e0d2697f2633c39802e0cf6e4e8
rawan <rmk69@mail.aub.edu>
2023-11-24 19:04:03 +0200

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.cultura {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
     requires transitive java.sql;
 
     opens com.cultura to javafx.fxml;
